0196ebe24cb378b8c85d2286774d97a2366ecc85
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-07-02 13:26:56 -0500

    157. Carrusel de posters

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,23 @@
 import { Text, View } from 'react-native';
 import { useMovies } from '../../hooks/useMovies';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PosterCarousel } from '../../components/movies/PosterCarousel';
 
 export const HomeScreen = () => {
-	const {} = useMovies();
+	const { top } = useSafeAreaInsets();
+
+	const { isloading, nowPlaying } = useMovies();
+
+	if (isloading) {
+		return <Text>Cargando...</Text>;
+	}
 
 	return (
-		<View>
-			<Text>HomeScreen</Text>
-		</View>
+		<ScrollView>
+			<View style={{ marginTop: top + 20, paddingBottom: 30 }}>
+				<PosterCarousel movies={nowPlaying} />
+			</View>
+		</ScrollView>
 	);
 };
