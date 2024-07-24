5c5d322b1ad770607eb2628e5b17b8079e346ada
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-07-24 11:58:58 -0500

    164. Pantalla de detalles - Header

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -2,16 +2,28 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import { RootStackParams } from '../../navigation/Navigation';
 import { useMovie } from '../../hooks/useMovie';
+import { MovieHeader } from '../../components/movie/MovieHeader';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
 export const DetailsScreen = ({ route }: Props) => {
 	const { movieId } = route.params;
 
-	const {} = useMovie(movieId);
+	const { isLoading, movie } = useMovie(movieId);
+
+	if (isLoading) {
+		return <Text>Loading</Text>;
+	}
 	return (
 		<View>
-			<Text>DetailsScreen</Text>
+			{/* Header */}
+			<MovieHeader
+				originalTitle={movie!.originalTitle}
+				title={movie!.title}
+				poster={movie!.poster}
+			/>
+
+			{/* Details */}
 		</View>
 	);
 };
